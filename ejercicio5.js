8da40968448f3c531bef10e81988ae9ccf772183
xaviza11 <xaviza11@gmail.com>
2022-09-06 12:32:50 +0200

    este es un comentario

---
+++ ejercicio5.js
@@ -0,0 +1,16 @@
+var numA = 10;
+var numB = 4;
+
+var sumResult = numA + numB;
+var restResult = numA - numB;
+var divResult = numA/numB;
+var multResult = numA * numB;
+
+console.log(sumResult, restResult, divResult, multResult);
+
+const name = "Xavier";
+const surname = "Zamora";
+var age = 25;
+var greeting = "Este es el texto adicional";
+
+console.log("mi nombre es", name, surname, "tengo", age, "años", "y", greeting);
